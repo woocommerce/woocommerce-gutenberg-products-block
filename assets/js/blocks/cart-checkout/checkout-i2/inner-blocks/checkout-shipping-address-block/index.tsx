762414f504452c06b1eda1ab52954df5818e09fb
Nadir Seghir <nadir.seghir@gmail.com>
2021-05-26 16:57:42 +0100

    frontend WIP

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/index.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/index.tsx
@@ -8,7 +8,7 @@ import { registerFeaturePluginBlockType } from '@woocommerce/block-settings';
  * Internal dependencies
  */
 import { Edit, Save } from './edit';
-import formStepAttributes from '../../form-step/attributes';
+import attributes from './attributes';
 
 registerFeaturePluginBlockType( 'woocommerce/checkout-shipping-address-block', {
 	title: __( 'Shipping Address', 'woo-gutenberg-products-block' ),
@@ -22,18 +22,7 @@ registerFeaturePluginBlockType( 'woocommerce/checkout-shipping-address-block', {
 		html: false,
 		multiple: false,
 	},
-	attributes: {
-		...formStepAttributes( {
-			defaultTitle: __(
-				'Shipping address',
-				'woo-gutenberg-products-block'
-			),
-			defaultDescription: __(
-				'Enter the address where you want your order delivered.',
-				'woo-gutenberg-products-block'
-			),
-		} ),
-	},
+	attributes,
 	apiVersion: 2,
 	edit: Edit,
 	save: Save,
