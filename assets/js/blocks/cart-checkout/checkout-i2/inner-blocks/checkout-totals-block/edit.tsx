91972ab6f60f64ffa644ebc39007b4043ff4ed26
Mike Jolley <mike.jolley@me.com>
2021-06-01 16:09:51 +0100

    Set correct parent blocks for inner blocks

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-totals-block/edit.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-totals-block/edit.tsx
@@ -23,7 +23,7 @@ export const Edit = (): JSX.Element => {
 				<InnerBlocks
 					allowedBlocks={ ALLOWED_BLOCKS }
 					template={ TEMPLATE }
-					templateLock={ 'insert' }
+					templateLock={ false }
 				/>
 			</div>
 		</Sidebar>
