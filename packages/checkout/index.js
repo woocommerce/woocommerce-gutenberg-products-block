a178166696c3852f29e0bd316ff287488af50e23
Nadir Seghir <nadir.seghir@gmail.com>
2021-06-01 16:14:52 +0100

    try/block-registry

--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -2,6 +2,7 @@ export * from './totals';
 export * from './utils';
 export * from './slot';
 export * from './registry';
+export * from './blocks-registry';
 export { default as ExperimentalOrderMeta } from './order-meta';
 export { default as ExperimentalOrderShippingPackages } from './order-shipping-packages';
 export { default as Panel } from './panel';
